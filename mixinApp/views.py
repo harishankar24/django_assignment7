4a9860157c07c66e5504190930c88a75a7769d59
Hari <hari@shankar.com>
2023-04-08 11:01:08 +0530

    CRUD operation completed

--- mixinApp/views.py
+++ mixinApp/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
+from .models import Course
+from .serializer import CourseSerializer
 
-# Create your views here.
+
+
+class CourseList(ListModelMixin, CreateModelMixin, GenericAPIView):
+    
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+    
+    def get(self, request):
+        return self.list(request)
+
+    def post(self, request):
+        return self.create(request)
+
+
+
+class CourseDetail(RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin, GenericAPIView):
+
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+    def get(self, request, pk):
+        return self.retrieve(request, pk)
+
+    def put(self, request, pk):
+        return self.update(request, pk)
+
+    def delete(self, request, pk):
+        return self.destroy(request, pk)
